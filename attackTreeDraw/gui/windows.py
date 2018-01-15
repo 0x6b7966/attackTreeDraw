19f6aab4336db1a9ebf88088af19f34e09bd398d
Daniel Fischer <daniel.fischer@codeevolution.de>
2018-01-15 17:47:43 +0100

    Adds window

--- attackTreeDraw/gui/windows.py
+++ attackTreeDraw/gui/windows.py
@@ -1,4 +1,5 @@
 from PyQt5.QtWidgets import QWidget
+from PyQt5 import QtCore, QtGui, QtWidgets
 
 
 class NodeEdit(QWidget):
@@ -7,6 +8,21 @@ class NodeEdit(QWidget):
 
         self.nodeItem = node
 
+        self.setupUi()
+
+        self.show()
+
+    def setupUi(self):
         self.resize(320, 240)
         self.setWindowTitle("Node: %s" % self.nodeItem.node.title)
-        self.show()
+        self.gridLayout = QtWidgets.QGridLayout(self)
+        self.tableView = QtWidgets.QTableView(self)
+        self.gridLayout.addWidget(self.tableView, 0, 0, 1, 1)
+        self.horizontalLayout = QtWidgets.QHBoxLayout()
+        self.ok = QtWidgets.QPushButton(self)
+        self.horizontalLayout.addWidget(self.ok)
+        self.cancel = QtWidgets.QPushButton(self)
+        self.horizontalLayout.addWidget(self.cancel)
+        self.gridLayout.addLayout(self.horizontalLayout, 1, 0, 1, 1)
+        self.ok.setText('Ok')
+        self.cancel.setText('Cancel')
