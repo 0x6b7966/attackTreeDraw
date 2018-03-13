f3a59e02843800153d685fe419a8d0b919dd8ebf
Daniel Fischer <daniel.fischer@codeevolution.de>
2018-03-13 17:28:05 +0100

    Adds icon for mac

--- attackTreeDraw/attacktreedraw.py
+++ attackTreeDraw/attacktreedraw.py
@@ -1,12 +1,17 @@
 import sys
 import traceback
 
+import os
+from PyQt5.QtGui import QIcon
 from PyQt5.QtWidgets import QApplication
 from gui.main import Main
 
 if __name__ == "__main__":
     try:
         app = QApplication(sys.argv)
+        includePath = os.path.dirname(os.path.abspath(__file__))
+        app.setWindowIcon(QIcon(os.path.join(includePath, 'gui/assets/icons/logo.png')))
+
         ex = Main()
         sys.exit(app.exec_())
     except Exception:
