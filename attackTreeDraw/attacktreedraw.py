67cc2044d7a0d522d5fe0f2cdd9dd58b472d6634
Daniel Fischer <daniel.fischer@codeevolution.de>
2018-03-15 23:27:06 +0100

    Adds code to suppress std output

--- attackTreeDraw/attacktreedraw.py
+++ attackTreeDraw/attacktreedraw.py
@@ -1,5 +1,4 @@
 import sys
-import traceback
 
 import os
 from PyQt5.QtGui import QIcon
@@ -7,16 +6,12 @@ from PyQt5.QtWidgets import QApplication
 from gui.main import Main
 
 if __name__ == "__main__":
-    try:
-        app = QApplication(sys.argv)
-        includePath = os.path.dirname(os.path.abspath(__file__))
-        app.setWindowIcon(QIcon(os.path.join(includePath, 'gui/assets/icons/logo.png')))
-
-        ex = Main()
-        sys.exit(app.exec_())
-    except Exception:
-        print(sys.exc_info())
-        print(traceback.format_exc())
-        exit(-1)
-
-
+    debug = True
+    if debug is False:
+        sys.stdout = os.devnull
+        sys.stderr = os.devnull
+    app = QApplication(sys.argv)
+    includePath = os.path.dirname(os.path.abspath(__file__))
+    app.setWindowIcon(QIcon(os.path.join(includePath, 'gui/assets/icons/logo.png')))
+    ex = Main()
+    sys.exit(app.exec_())
